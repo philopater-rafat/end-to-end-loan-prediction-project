ab657693961dc0ed6b26d984a4d3cee093d585b2
philopater <philorafat2002@gmail.com>
2025-06-22 02:40:30 +0300

    docker setup

--- app/app.py
+++ app/app.py
@@ -51,4 +51,5 @@ def predict():
         return render_template("result.html", prediction=f"Error: {str(e)}")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
